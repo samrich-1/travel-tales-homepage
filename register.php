b8c76c4685fc1e6118d3e43f135e39993abfbb6b
Samrich <samrichke@gmail.com>
2025-04-09 10:47:48 +0300

    Add files via upload

--- register.php
+++ register.php
@@ -3,6 +3,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $username = $_POST['username'];
   $email = $_POST['email'];
   $password = $_POST['password'];
-  echo "User $username has been registered with the email $email.";
+  
+  // Register the user (you may want to add code to save the user details to a database)
+
+  // Redirect to login page after registration
+  header("Location: login.html");
+  exit();
 }
 ?>
